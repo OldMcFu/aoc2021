fac73581817143025ecdcafb184715d020ddd3cb
OldMcFu <darkst0rm@web.de>
2021-12-06 21:27:53 +0100

    Add other days

--- oldmcfu/Day4/task1/main.adb
+++ oldmcfu/Day4/task1/main.adb
@@ -1,104 +1,126 @@
-with Ada.Text_IO, Ada.Containers.Indefinite_Vectors, Ada.Strings.Fixed, Ada.Strings.Maps;
-use Ada.Text_IO, Ada.Containers, Ada.Strings, Ada.Strings.Fixed, Ada.Strings.Maps;
+with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Containers.Vectors;
+with Ada.Containers;
+with Ada.Strings.Fixed;
+with Ada.Strings.Maps;
+with Ada.Strings;
 
-with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+with Ada.Strings.Unbounded;
 
 procedure Main is
-    package String_Vectors is new Indefinite_Vectors (Positive, String);
-    use String_Vectors;
-   
-    F    : File_Type;
-    File_Name : constant String := "input";
-    Line :  Unbounded_String;
-    Str_Vec : Vector := Empty_Vector;
-    Match_Cnt : Integer := 0;
-    Array_Index : Integer := 0;
-    type Unbounded_String_Array_Type is array(0 .. 4) of Unbounded_String;
-    Buffer : Unbounded_String_Array_Type;
-    Sub : Integer := 0;
-    Last_Match_Number : Unbounded_String;
 
-    procedure Tokenize(Input : in String; Deliminiter : in Character; Output : in out Vector) is
-        Start  : Positive := Input'First;
-        Finish : Natural  := 0;
-        begin
-        while Start <= Input'Last loop
-            Find_Token (Input, To_Set (Deliminiter), Start, Outside, Start, Finish);
+    type Bingo_Field_Type is array (1 .. 5, 1 .. 5) of Natural;
+    package Bingo_Field_Vectors is new
+        Ada.Containers.Vectors
+        (Index_Type   => Natural,
+            Element_Type => Bingo_Field_Type);
+
+    package Natural_Vectors is new
+        Ada.Containers.Vectors 
+        (   Index_Type => Natural,
+            Element_Type => Natural);
+
+    
+    Bingo_Numbers : Natural_Vectors.Vector := Natural_Vectors.Empty_Vector;
+    Bingo_Fields  : Bingo_Field_Vectors.Vector := Bingo_Field_Vectors.Empty_Vector;
+    Sum : Natural := 0;
+
+    procedure Read_File(
+        Bingo_Numbers : in out Natural_Vectors.Vector;
+        Bingo_Fields  : in out Bingo_Field_Vectors.Vector
+        ) is
+        use Ada.Strings.Maps;
+        use Ada.Strings.Unbounded;
+        use Ada.Strings.Fixed;
+        use Ada.Strings;
+        F           : File_Type;
+        File_Name   : constant String := "input";
+        Line        : Unbounded_String;
+        Start       : Positive;
+        Finish      : Natural;
+        tmp : Bingo_Field_Type;
+        first_dim_cnt : Natural := 1;
+        First_Empty_Line : Boolean := True;
+        
+    begin
+        
+        Open (F, In_File, File_Name);
+        Line    := To_Unbounded_String (Get_Line (F));
+        Start   := To_String (Line)'First;
+        Finish  := 0;
+
+        while Start <= To_String (Line)'Last loop
+            Find_Token (Line, To_Set(','), Start, Ada.Strings.Outside, Start, Finish);
             exit when Start > Finish;
-            Output.Append (Input (Start .. Finish));
+            Bingo_Numbers.Append (Natural'Value(To_String (Line)(Start .. Finish)));
             Start := Finish + 1;
         end loop;
-    end Tokenize;
-    
-    First : Boolean := True;
+
+        loop
+            Line := To_Unbounded_String (Get_Line (F));
+            declare
+                second_dim_cnt : Natural := 1;
+            begin
+                if To_String (Line) = "" then
+                    second_dim_cnt  := 1;
+                    first_dim_cnt := 1;
+                    if First_Empty_Line = False then 
+                        Bingo_Fields.Append (tmp);
+                    end if;
+                    First_Empty_Line := False;
+                else
+                    Start := To_String (Line)'First;
+                    Finish  := 0;
+                    while Start <= To_String (Line)'Last loop
+                        Find_Token (Line, To_Set(' '), Start, Ada.Strings.Outside, Start, Finish);
+                        exit when Start > Finish;
+                        tmp(first_dim_cnt, second_dim_cnt) := Natural'Value(To_String (Line)(Start .. Finish));
+                        Start := Finish + 1;
+                        second_dim_cnt := second_dim_cnt + 1;
+                    end loop;
+                    first_dim_cnt := first_dim_cnt + 1;                
+                end if;
+            end;
+            exit when End_Of_File (F);
+        end loop;
+        Close(F);
+    end Read_File;
+
 begin
 
-    Open (F, In_File, File_Name);
-    Main_Cycle:
-    while not End_Of_File (F) loop
-        Line := To_Unbounded_String (Get_Line (F));
-        if First = True then
-            Tokenize (To_String(Line), ',', Str_Vec);
-            First := False;
-        else
-            if To_String (Line) = "" then
-                Array_Index := 0;
-                exit Main_Cycle when Match_Cnt = 5;
-            else
-                Buffer(Array_Index) := Line;
-                Array_Index := Array_Index + 1;
-                Match_Cnt := 0;
-                for S of Str_Vec loop
-                    declare
-                        Max_Match_Cnt : constant Integer := 5;
-                    begin
-                        if Index (To_String(Line), S) > 0 then
-                            Match_Cnt := Match_Cnt + 1;
-                            if Match_Cnt = Max_Match_Cnt then
-                                Put_Line ("Last Match Number is: " & S);
-                                Put_Line ("Line String is: " & To_String(Line));
-                                Last_Match_Number := To_Unbounded_String (S);
-                                while To_String (Line) /= "" loop
-                                    Line := To_Unbounded_String (Get_Line (F));
-                                    If To_String (Line) /= "" then 
-                                        Buffer(Array_Index) := Line;
-                                        Array_Index := Array_Index + 1;
-                                    end if;
-                                end loop;
-                                exit Main_Cycle when To_String (Line) = "";
-                            end if;
-                        end if;
-                    end;
-                end loop;
-            end if;      
-        end if;
-    end loop Main_Cycle; 
-    Close (F);
+    Read_File (Bingo_Numbers, Bingo_Fields);
 
-    declare
-        Sum : Integer := 0;
-        Vec: Vector;
-        Match : Boolean := False;
-    begin
-        for S of Buffer loop
-            Tokenize (To_String(S), ' ', Vec);
-            for I of Vec loop
-                Loop_Over_Header:
-                for X of Str_Vec loop
-                    if X = I then 
-                        Match := True;
+    Main_Loop:
+    for BN of Bingo_Numbers loop
+        for BF of Bingo_Fields loop
+            for Row in 1 .. 5 loop
+                Sum := 0;
+                for Col in 1 .. 5 loop
+                    if BN = BF(Row,Col) then 
+                        BF(Row,Col) := 0;
                     end if;
-                    exit Loop_Over_Header when X = Last_Match_Number;
-                end loop Loop_Over_Header;
-                if Match = False then Sum := Sum + Integer'Value(I); end if;
-                Match := False;
+                    Sum := Sum + BF(Row,Col);
+                    if Sum = 0 and Col = 5 then
+                        Put_Line ("------------------------------");
+                        Put_Line ("---------BINGO----------------");
+                        Put_Line ("------------------------------");
+                        Put_Line ("Last Bingo Number: " & Natural'Image(BN));
+                        
+                        for X in 1 .. 5 loop
+                            for XX in 1 .. 5 loop
+                                Put (" " & Natural'Image(BF(X,XX)) & " ");
+                                Sum := Sum + BF(X,XX); 
+                            end loop;
+                        Put_Line ("");
+                        end loop;
+                        Put_Line("");
+                        Put_Line("Sum: " & Natural'Image(Sum));
+                        Put_Line("Result = Sum * Called_Number: " & Natural'Image(Sum*BN));
+                        exit Main_Loop;
+                    end if;
+                end loop;
             end loop;
         end loop;
+    end loop Main_Loop;
 
-        Sum := Sum - Sub;
-        Sum := Sum * Integer'Value(To_String(Last_Match_Number));
-
-        Put_Line ("Sum is: " & Integer'Image(Sum));
-
-    end;
 end Main;
